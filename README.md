b610d1178892db94822c6a0f00ac71a89801d572
lsefiane <lyes.sefiane@gmail.com>
2021-10-14 14:18:05 -0400

    ~ Spring Security Keycloak Config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Data Visualization Microservices
+# Graph Visualization
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- ui-service/src/main/java/com/lsefiane/ui/controller/WebController.java
+++ ui-service/src/main/java/com/lsefiane/ui/controller/WebController.java
@@ -1,17 +1,34 @@
 package com.lsefiane.ui.controller;
 
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/")
+@PreAuthorize("isAuthenticated()")
 public class WebController {
 
     @GetMapping(value = { "/", "/index" })
-    public String getHomePage(Model model) {
+    public String getHomePage(Model model, Principal principal) {
+        KeycloakAuthenticationToken keycloakAuthenticationToken = (KeycloakAuthenticationToken) principal;
+        AccessToken accessToken = keycloakAuthenticationToken.getAccount().getKeycloakSecurityContext().getToken();
+        model.addAttribute("userInfo", accessToken);
         return "index";
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
     }
 
 
